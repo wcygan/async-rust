ef63b2d7993d7a5d5961e6f3d8b93c40f3995683
Will Cygan <wcygan.io@gmail.com>
2025-11-01 18:09:51 -0500

    panic

--- 10-pinning-in-futures/src/main.rs
+++ 10-pinning-in-futures/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
+use std::ptr;
+
+/// In Rust, the compiler often moves values around in memory.
+/// For instance, if we move a variable into a function,
+/// the memory may be moved.
+/// 
+/// It's not just moving values that may result in moving
+/// memory addresses. Collection can also change memory addresses.
+/// For instance, if a vector gets to capacity, the vector
+/// will be reallocated in memory, changing the memory address.
+/// 
+/// Most normal primitives such as number, string, bool, structs,
+/// and enum implement the `Unpin` trait, enabling them to be
+/// moved around.
+/// 
+/// We know that Futures can get moved, as we use `async move` in
+/// our code when spawning a task. However, moving can be
+/// dangerous. To demonstrate the data, we can build
+/// a basic struct that references itself:
+
+struct SelfReferential {
+    data: String,
+    self_pointer: *const String
+}
+
+impl SelfReferential {
+    fn new(data: String) -> SelfReferential {
+        let mut sr = SelfReferential {
+            data,
+            self_pointer: ptr::null()
+        };
+        sr.self_pointer = &sr.data as *const String;
+        sr
+    }
+    
+    fn print(&self) {
+        unsafe {
+            println!("{}", *self.self_pointer);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let first = SelfReferential::new("first".to_string());
+    let moved_first = first;
+    moved_first.print();
 }
