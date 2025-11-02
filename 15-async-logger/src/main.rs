ac16ef33d64915e68909a338f5321b3ebb735d31
Will Cygan <wcygan.io@gmail.com>
2025-11-02 10:45:06 -0600

    openoptions

--- 15-async-logger/src/main.rs
+++ 15-async-logger/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::sync::{Arc, Mutex};
 use tokio::task::JoinHandle;
 
@@ -22,8 +22,33 @@ use tokio::task::JoinHandle;
 /// user's actions, but we do not want that logging action to hold up the
 /// program because we still want to facilitate a quick user experience.
 
+/// Seeing as we do not want two tasks writing to the file at the same time,
+/// it makes sense to use a Mutex to ensure that only one task is writing to the file
+/// at any given time.
+///
+/// We also might want to write to multiple files. For instance, we might want to
+/// write all logins to one file, and error messages to another file. If you have
+/// medical patients in your system, you want to have a log file per patient (as
+/// you would probably inspect log files on a per-patient basis), and you'd want to
+/// prevent unauthorized people looking at actions on a patient that they are
+/// not allowed to view.
 type AsyncFileHandle = Arc<Mutex<File>>;
 type FileJoinHandle = JoinHandle<Result<bool, String>>;
+
+/// Considering there are needs for multiple files when logging, we can create a
+/// function that creates a file or obtains the handle of an existing file:
+fn get_file_handle(file_path: &dyn ToString) -> AsyncFileHandle {
+    match OpenOptions::new().append(true).open(file_path.to_string()) {
+        Ok(opened_file) => {
+            Arc::new(Mutex::new(opened_file))
+        }
+        Err(_) => {
+            let created_file = File::create(file_path.to_string())
+                .expect("Failed to create log file");
+            Arc::new(Mutex::new(created_file))
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
