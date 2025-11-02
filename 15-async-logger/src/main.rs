1f31b645ba4b9e5f96904ca978d4c625763927f0
Will Cygan <wcygan.io@gmail.com>
2025-11-02 10:32:16 -0600

    deps

--- 15-async-logger/src/main.rs
+++ 15-async-logger/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+/// This program will use ideas from the past few programs
+/// {10, 11, 12, 13, 14} to create an asynchronous logger
+/// that writes log messages to a file without blocking the main thread.
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
