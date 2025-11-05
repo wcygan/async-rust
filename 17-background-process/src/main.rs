85ea98fd04667c29cafad6dafc710e607ac2d7c6
Will Cygan <wcygan.io@gmail.com>
2025-11-04 21:05:31 -0600

    background process

--- 17-background-process/src/main.rs
+++ 17-background-process/src/main.rs
@@ -1,4 +1,22 @@
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    tokio::spawn(BackgroundProcess {});
+    std::thread::sleep(std::time::Duration::from_secs(5));
+}
+
+#[derive(Debug, Clone, Copy)]
+struct BackgroundProcess {}
+
+impl Future for BackgroundProcess {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("Poll called on background process");
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        cx.waker().wake_by_ref();
+        Poll::Pending
+    }
 }
